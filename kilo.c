e4fd3ed1f9fe1a00fb1a0032b334b6205b9e97ed
Apurv <apurvmishra99@gmail.com>
2019-08-06 11:09:03 +0530

    added debugging scripts for vscode and more changes

--- kilo.c
+++ kilo.c
@@ -14,6 +14,8 @@
 
 /*** data ***/
 struct editorConfig {
+    int screenRows;
+    int screenCols;
     struct termios orig_termios;
 };
 
@@ -67,11 +69,40 @@ char editorReadKey()
 }
 
 
+int getCursorPosition(int *rows, int *cols) {
+    char buf[32];
+    unsigned int i = 0;
+
+
+    if (write(STDOUT_FILENO, "\x1b[6n", 4) != 4)
+        return -1;
+
+    while (i < sizeof(buf) - 1) {
+        if (read(STDIN_FILENO, &buf[i], 1) != 1)
+            break;
+        if (buf[i] == 'R')
+            break;
+        i++;
+    }
+    buf[i] = '\0';
+
+    if (buf[0] != '\x1b' || buf[1] != '[')
+        return -1;
+    if (sscanf(&buf[2], "%d;%d", rows, cols) != 2)
+        return -1;
+    
+    return 0;
+} 
+
+
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
 
-    if (ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
-        return -1;
+    if (1 || ioctl(STDOUT_FILENO, TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
+        
+        if (write(STDOUT_FILENO, "\x1b[999C\x1b[999B", 12) != 12)
+            return -1;
+        return getCursorPosition(rows, cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
@@ -83,7 +114,7 @@ int getWindowSize(int *rows, int *cols) {
 
 void editorDrawRows() {
     int y; 
-    for (y = 0; y < 24; y++)
+    for (y = 0; y < E.screenRows; y++)
     {
         write(STDOUT_FILENO, "~\r\n", 3);
     }
@@ -116,8 +147,14 @@ void editorProcessKeypress() {
 
 /*** init ***/
 
-int main() {
+void initEditor() {
+    if (getWindowSize(&E.screenRows, &E.screenCols) == -1)
+        die("getWindowSize");
+}
+
+int main(int argc, char **argv) {
     enableRawMode();
+    initEditor();
 
     while(1) {
         editorRefreshScreen();
